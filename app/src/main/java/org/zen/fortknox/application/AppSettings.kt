3f8288546e0567f6472a17b0035a67a364a4caf2
mehdiprgm <zendevmehdi@gmail.com>
2025-11-26 19:41:36 +0330

    Added new settings to the class

--- app/src/main/java/org/zen/fortknox/application/AppSettings.kt
+++ app/src/main/java/org/zen/fortknox/application/AppSettings.kt
@@ -2,7 +2,13 @@ package org.zen.fortknox.application
 
 import org.zen.fortknox.tools.theme.Theme
 
-/* Confirm exit             -> User put some information on form and wants to exit, so user should confirm it first */
+/* theme                    -> Application default theme */
+/* confirmExit              -> User put some information on form and wants to exit, so user should confirm it first */
+/* allowScreenshot          -> Allow user to take screenshot of screens contains sensitive information */
+/* lockTimeout              -> How many seconds take to allow user to enter the application passcode after 3 times failed attempt */
 data class AppSettings(
-    var theme: Theme, var confirmExit: Boolean
+    var theme: Theme,
+    var confirmExit: Boolean,
+    var allowScreenshot: Boolean,
+    var lockTimeout: Int
 )
