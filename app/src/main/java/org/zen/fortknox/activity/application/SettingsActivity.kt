65f1d6297ceec4b186309e5c3fd6852a25adae29
mehdiprgm <zendevmehdi@gmail.com>
2025-11-27 10:10:45 +0330

    new settings activity

---
+++ app/src/main/java/org/zen/fortknox/activity/application/SettingsActivity.kt
@@ -0,0 +1,30 @@
+package org.zen.fortknox.activity.application
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.zen.fortknox.R
+import org.zen.fortknox.databinding.ActivitySettingsBinding
+import org.zen.fortknox.tools.disableScreenPadding
+
+class SettingsActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var b: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        b = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(b.root)
+        disableScreenPadding(b.root)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+
+        }
+    }
+}
