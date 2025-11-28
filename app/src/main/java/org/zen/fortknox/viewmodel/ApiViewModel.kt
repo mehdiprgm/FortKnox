2841237eaeaecde37980abff5d8be770a2f9f5c0
mehdiprgm <zendevmehdi@gmail.com>
2025-11-28 19:44:52 +0330

    add new method to upload user image

--- app/src/main/java/org/zen/fortknox/viewmodel/ApiViewModel.kt
+++ app/src/main/java/org/zen/fortknox/viewmodel/ApiViewModel.kt
@@ -6,6 +6,7 @@ import org.zen.fortknox.api.entity.ApiUser
 import org.zen.fortknox.api.respository.ApiEmailRepository
 import org.zen.fortknox.api.respository.ApiUserRepository
 import org.zen.fortknox.api.base.RetrofitClient
+import java.io.File
 
 class ApiViewModel : ViewModel() {
     private var apiUserRepository: ApiUserRepository = ApiUserRepository(RetrofitClient.userService)
@@ -25,6 +26,10 @@ class ApiViewModel : ViewModel() {
 
     suspend fun getAllUsers(): Result<List<ApiUser>> {
         return apiUserRepository.getAll()
+    }
+
+    suspend fun uploadUserImage(localFile: File, fileName: String) : Result<String> {
+        return apiUserRepository.uploadUserImage(localFile, fileName)
     }
 
     suspend fun sendVerificationEmail(apiEmail: ApiEmail): Result<ApiEmail> {
