b885049eef9802bfb6ce3f430b0a891da3b33ffd
mehdiprgm <zendevmehdi@gmail.com>
2025-11-28 19:36:13 +0330

    new utils file for working with api

--- app/src/main/java/org/zen/fortknox/tools/ApiUtils.kt
+++ app/src/main/java/org/zen/fortknox/tools/ApiUtils.kt
@@ -6,10 +6,8 @@ import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 
 private fun prepareFilePart(file: File, partName: String): MultipartBody.Part {
-    // تعیین نوع مدیا (MediaType) بر اساس پسوند فایل یا نوع آن
-    val mediaType = "image/*".toMediaTypeOrNull() // باید دقیق‌تر باشد، مثلاً image/jpeg.
+    val mediaType = "image/*".toMediaTypeOrNull()
     val requestBody = file.asRequestBody(mediaType)
 
-    // ساخت Part اصلی با کلید "file" (که Flask انتظارش را دارد)
     return MultipartBody.Part.createFormData(partName, file.name, requestBody)
 }
